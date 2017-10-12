f41008f8a9a53bb5021f6ba2f3db7494af48364b
Aaron24 <31861699+DaveGopal24@users.noreply.github.com>
2017-10-12 12:55:40 -0500

    Assignment 4
    Aaron Pandian
    October 12, 2017
    Mr. Hudson
    Period 3

--- Project2/Project2/Three_digit_ascend_descend_selection.cpp
+++ Project2/Project2/Three_digit_ascend_descend_selection.cpp
@@ -1,37 +1,53 @@
 /*
 Julian Haddad - 10/5/17 Period 3
 Assignment Name :  Three Digit Ascend/Descend
-
-Three Digit Ascend/Descend 
+Three Digit Ascend/Descend
 */
+
 #include <iostream>
 #include <conio.h>
 
 using namespace std;
 
-void pause() { 
+void pause() {
 	while (!_kbhit());
 	_getch();
 	cout << "\n";
-} 
-void main() {
-	int x, a, b, c;
-	cout << "Enter a 3 digit number . . ." << endl;
+}
+
+void main() {	
+for (size_t i = 0; i < 30; i++) {
+		
+	int x;
+
+	cout << "Enter a 3 digit number : ";
 	cin >> x; // This is where the user inputs the 3 digit number
 
-	a = x / 100; // This first digit
-	
-	b = (x - (a * 100)) / 10; // This is the second digit
-	
-	c = ((x - (a * 100)) - (b * 10)); // This is the third digit
-	
+		int a;
+		int b;
+		int c;
+		
+		a = x / 100; // This first digit
 
-	if (a < b && b < c){ // This checks if the first digit is smaller then the second and if the second is smaller than the third
-		cout << "Ascending";}
-	else if (a > b && b > c) { // this checks if the first digit is larger then the second and if the second is larger than the third
-		cout << "Descending";}
-	else {
-		cout << "Neither"; // This is only used if the number does not apply to the "if" or the "else if"
+		b = (x / 10) % 10; // This is the second digit
+
+		c = x % 10; // This is the third digit
+
+
+		if (a < b && b < c) { // This checks if the first digit is smaller then the second and if the second is smaller than the third, if it meets condition prints out the word ascending
+			cout << x << " is Ascending." "\n";
+		}
+
+		else if (a > b && b > c) { // this checks if the first digit is larger then the second and if the second is larger than the third, if it meets condition prints out word descending
+			cout << x << " is Descending." "\n";
+		}
+
+		else {
+			cout << x << " is Neither." "\n";
+		} // This is only used if the number does not apply to the "if" or the "else if", applys to everythin else not meet in conditions above, if meet prints out neither
+
 	}
+	
 	pause();
+
 }
